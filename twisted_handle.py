54b036ca1f0c15db407dc5653a0f37eebf9f93a9
unknown <yangx@utek.com>
2018-06-06 15:17:32 +0800

    注释掉twisted_handle模块，改用selector模块

--- twisted_handle.py
+++ twisted_handle.py
@@ -1,77 +1,84 @@
-from PyQt5 import QtCore
+# from PyQt5 import QtCore
 
-from twisted.internet.protocol import Factory
-from twisted.protocols.basic import LineReceiver
+# from twisted.internet.protocol import Factory, Protocol
+# from twisted.internet import reactor, task
+# import struct
+# from twisted.python import log
+# import sys
+# import time
+# log.startLogging(sys.stdout)
 
+# class TcpServerPipe(QtCore.QObject):
+#     """
+#     twisted和qt页面通信类
+#     """
+#     pipeSignal = QtCore.pyqtSignal(str)
 
-class TcpServerPipe(QtCore.QObject):
-    """
-    twisted和qt页面通信类
-    """
-    pipeSignal = QtCore.pyqtSignal()
+#     def emitData(self, data):
+#        self.pipeSignal.emit(data) 
 
-    def emitData(self, data):
-       self.pipeSignal.emit(data) 
+# global tcp_server_pipe
+# tcp_server_pipe = TcpServerPipe()
 
-global tcp_server_pipe
-tcp_server_pipe = TcpServerPipe()
+# class Work(Protocol):
+#     def __init__(self, users):
+#         self.users = users
+#         # self.last_heartbeat_time = 0
+#         self._data_buffer = bytes()
 
-class Work(LineReceiver):
-    def __init__(self):
-        self.state = "GETNAME"
+#     def connectionMade(self):
+#         log.msg("New connect, the info is:",
+#                 self.transport.getPeer())
+#         global tcp_server_pipe
+#         tcp_server_pipe.emitData(self.transport.getPeer())
 
-    def connectionMade(self):
-        echo_buf = "Welcome to the UTEK network debugging tool."
-        self.sendLine(echo_buf.encode('utf-8'))
+#     def connectionLost(self, reason):
+#         if self.phone_number in self.users:
+#             del self.users[self.phone_number]
 
-    def connectionLost(self):
-        if self.name in self.users:
-            del self.users[self.name]
+#     def dataReceived(self, data):
+#         global tcp_server_pipe
+#         tcp_server_pipe.emitData(data)
 
-    def lineReceived(self, line):
-        if self.state == 'GETNAME':
-            self.handle_GETNAME(line)
-        else:
-            self.handle_CHAT(line)
-    
-    def handle_GETNAME(self, name):
-        if name in self.users:
-            self.sendLine("Name taken, please choose anothre.".encode("utf-8"))
-            return
-        self.sendLine(("Welcome, %s" % name).encode('utf-8'))
-        self.name = name
-        self.users[name] = self
-        self.state = "CHAT"
+#     # def handle_heartbeat(self, content):
+#     #     self.last_heartbeat_time = int(time.time())
 
-    def handle_CHAT(self, message):
-        message = "<%s> %s" % (
-            self.name.decode('utf-8', 'ignore'),
-            message.decode('utf-8', 'ignore')
-        )
-        for _, protocol in self.users.items():
-            if protocol != self:
-                protocol.sendLine(message.encode('utf-8'))
-
-
-class WorkFactory(Factory):
-    def __init__(self):
-        print('starting....')
-        self.users = {}
-
-    def buildProtocol(self, addr):
-        return Work(self.users)
+#     def send_content(self, send_content, command_id, phone_numbers):
+#         length = 12 + len(send_content)
+#         version = self.version
+#         command_id = command_id
+#         header = [length, version, command_id]
+#         header_pack = struct.pack('!3I', *header)
+#         for phone_number in phone_numbers:
+#             if phone_number in self.users.keys():
+#                 self.users[phone_number].transport.write(
+#                     header_pack + send_content
+#                 )
+#             else:
+#                 log.msg(
+#                     "Phone_number:%s 不在线，不能聊天" % phone_number.encode('utf-8')
+#                 )
 
 
-class TcpServerDebug(QtCore.QThread):
-    receive_data = QtCore.pyqtSignal()
-    def __int__(self):
-        super(TcpServerDebug, self).__init__()
+# class WorkFactory(Factory):
+#     def __init__(self):
+#         print('starting....')
+#         self.users = {}
 
-    def setPort(self, port):
-        self._port = port
+#     def buildProtocol(self, addr):
+#         return Work(self.users)
 
-    def run(self):
-        from twisted.internet import reactor
-        reactor.listenTCP(self._port, ChatFactory())
-        reactor.run()
-        del reactor
+
+# class TcpServerDebug(QtCore.QThread):
+#     receive_data = QtCore.pyqtSignal()
+#     def __int__(self):
+#         super(TcpServerDebug, self).__init__()
+
+#     def setPort(self, port):
+#         self._port = port
+
+#     def run(self):
+#         from twisted.internet import reactor
+#         reactor.listenTCP(self._port, ChatFactory())
+#         reactor.run()
+#         del reactor
